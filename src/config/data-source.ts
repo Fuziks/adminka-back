66dc479ecc3c25721f0eb8ccfccfe8395deed7e9
Artem Korovin <artemkorovin891@mail.ru>
2025-06-17 23:39:27 +0300

    render loading

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -8,14 +8,20 @@ dotenv.config({ path: resolve(__dirname, '../.env') });
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'db',
+  host: process.env.DB_HOST || 'dpg-d16d6a6mcj7s73c0u000-a.frankfurt-postgres.render.com',
   port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_NAME || 'pc_components',
+  username: process.env.DB_USERNAME || 'postgres1',
+  password: process.env.DB_PASSWORD || 'G0xmciPahwAa4G0AbQ9bmrBAxFGGq5gv',
+  database: process.env.DB_NAME || 'pc_components_xnjj',
   entities: [Category, Product], 
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
   synchronize: false, 
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false, 
+    },
+  },
   logging: true, 
   migrationsRun: true, 
 });
